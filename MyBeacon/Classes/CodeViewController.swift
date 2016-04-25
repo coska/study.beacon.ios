052503eb8702f1f2378a6cf0cf7b4f4855e15050
Dexter Kim <dexterk@ecobee.com>
2016-04-24 23:43:15 -0400

    add code style example

--- MyBeacon/Classes/CodeViewController.swift
+++ MyBeacon/Classes/CodeViewController.swift
@@ -8,40 +8,101 @@
 
 import UIKit
 
-class CodeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+// enum
+enum SectionInfo: Int {
+    case BeaconName
+    case BasicInfo
+    case AdditionalInfo
+    
+    var numOfRows: Int {
+        switch self {
+        case .BeaconName: return 1
+        case .BasicInfo: return 2
+        case .AdditionalInfo: return 3
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .BeaconName: return "Beacon Name"
+        case .BasicInfo: return "Basic Information"
+        case .AdditionalInfo: return "Additional Information"
+        }
+    }
+}
+
+class CodeViewController: UIViewController {
 // Open curly bracket is at the begining of a line.
-{
+
     // variable
-    var tableView: UITableView?
+    // Getter
+    var readOnlyProperty: Bool {
+        return true
+    }
+    // Setter
+    var setterProperty: String {
+        didSet {
+            if self.readOnlyProperty == true {
+                setterProperty = "true"
+            } else {
+                setterProperty = "false"
+            }
+            print("\(setterProperty)")
+        }
+    }
+    // Optional
+    var optionalProperty: String?
+    // Lazy
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }()
     
     // IBOutlet
     
     // MARK:- View life cycle
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView = UITableView()
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
+        initializeTableVC()
     }
 
-    override func didReceiveMemoryWarning()
-    {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     // MARK:- Public
+    func regularFunction(first: Int, second: String) -> String {
+        
+        // guard
+        guard first > 0 else { return "" }
+        
+        return ""
+    }
     
     // MARK:- Even handler
     
     // MARK:- Notification
 
     // MARK:- Private
+    private func privateFunction() {
+        
+    }
     
+    // MARK:- Internal
+    internal func initializeVC() {
+        
+    }
+}
+
+extension CodeViewController: UITableViewDataSource {
     // MARK:- UITableViewDataSource
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
-    {
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 3
     }
+}
+
+extension CodeViewController: UITableViewDelegate {
+    
 }
