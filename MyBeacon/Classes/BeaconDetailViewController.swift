8e784c4a43d3c02c1c98b8b6797b78adf2777a0b
Dexter Kim <dexterk@ecobee.com>
2016-05-02 23:41:18 -0400

    code clean up

--- MyBeacon/Classes/BeaconDetailViewController.swift
+++ MyBeacon/Classes/BeaconDetailViewController.swift
@@ -242,6 +242,10 @@ extension BeaconDetailViewController: UITableViewDelegate {
             return cell
         }
     }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
 
 extension BeaconDetailViewController: BeaconProtocol {
