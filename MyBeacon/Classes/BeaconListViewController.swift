7d1a10b67153709591b09157449fc443cb1dfabf
Danny Lee <danny@sigakorea.com>
2016-06-05 20:48:48 -0400

    Beacon Image Changed

--- MyBeacon/Classes/BeaconListViewController.swift
+++ MyBeacon/Classes/BeaconListViewController.swift
@@ -14,17 +14,71 @@ class BeaconListViewController: UIViewController, UITableViewDataSource, UITable
 
     lazy var fakeDataSource: Array<String> =
     {
-        return ["Alex's Beacon", "Dexter's Beacon", "Thomas's Beacon"]
+        return [
+            "BeaconStac",
+            "Air Locate",
+            "Beacon Inside",
+            "Robin",
+            "Open Beacon",
+            "Estimote",
+            "Kontakt.io",
+            "Radius Networks",
+            "Roximity",
+            "BlueBeacon",
+            "Glimworm",
+            "Bluecats",
+            "Passkit",
+            "Bleu Station",
+            "EasiBeacon",
+            "RedBear"
+            // Gimbal
+        ]
     }()
     
     lazy var fakeImageSource: Array<String> =
     {
-       return ["canada.png", "germany.png", "uk.png"]
+       return [
+        "F94DBB23-2266-7822-3782-57BEAC0952AC.png", // BeaconStac
+        "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5.png", // Air Locate
+        "F0018B9B-7509-4C31-A905-1A27D39C003C.png", // Beacon Inside
+        "44F77920-EBF9-11E3-AC10-0800200C9A66.png", // Robin
+        "AA6062F0-98CA-4211-8EC4-193EB73CEBE6.png", // Open Beacon
+        "B9407F30-F5F8-466E-AFF9-25556B57FE6D.png", // Estimote
+        "F7826DA6-4FA2-4E98-8024-BC5B71E0893E.png", // Kontakt.io
+        "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6.png", // Radius Networks
+        "8DEEFBB9-F738-4297-8040-96668BB44281.png", // Roximity
+        "acfd065e-c3c0-11e3-9bbe-1a514932ac01.png", // BlueBeacon
+        "74278BDA-B644-4520-8F0C-720EAF059935.png", // Glimworm
+        "61687109-905F-4436-91F8-E602F514C96D.png", // Bluecats
+        "19D5F76A-FD04-5AA3-B16E-E93277163AF6.png", // Passkit
+        "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0.png", // Bleu Station
+        "EBEFD083-70A2-47C8-9837-E7B5634DF524.png", // EasiBeacon
+        "B0702980-A295-A8AB-F734-031A98A512DE.png"  // RedBear
+                                                    // Gimbal
+        ]
     }()
     
     lazy var fakeUUIDSource: Array<String> =
     {
-        return ["F94DBB23-2266-7822-3782-57BEAC0952AC", "F94DBB23-2266-7822-3782-57BEAC0952AC", "F94DBB23-2266-7822-3782-57BEAC0952AC"]
+        return [
+            "F94DBB23-2266-7822-3782-57BEAC0952AC", // BeaconStac
+            "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", // Air Locate
+            "F0018B9B-7509-4C31-A905-1A27D39C003C", // Beacon Inside
+            "44F77920-EBF9-11E3-AC10-0800200C9A66", // Robin
+            "AA6062F0-98CA-4211-8EC4-193EB73CEBE6", // Open Beacon
+            "B9407F30-F5F8-466E-AFF9-25556B57FE6D", // Estimote
+            "F7826DA6-4FA2-4E98-8024-BC5B71E0893E", // Kontakt.io
+            "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6", // Radius Networks
+            "8DEEFBB9-F738-4297-8040-96668BB44281", // Roximity
+            "acfd065e-c3c0-11e3-9bbe-1a514932ac01", // BlueBeacon
+            "74278BDA-B644-4520-8F0C-720EAF059935", // Glimworm
+            "61687109-905F-4436-91F8-E602F514C96D", // Bluecats
+            "19D5F76A-FD04-5AA3-B16E-E93277163AF6", // Passkit
+            "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0", // Bleu Station
+            "EBEFD083-70A2-47C8-9837-E7B5634DF524", // EasiBeacon
+            "B0702980-A295-A8AB-F734-031A98A512DE"  // RedBear
+                                                    // Gimbal
+        ]
     }()
     
     @IBOutlet weak var tableView: UITableView!
